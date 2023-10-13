75c5aed654c16c033c047d953d25cf795f8a7e87
Amie <molin@FIZZYGOOD_3060>
2023-10-13 15:54:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LAB5_NET23Amie_GrunderOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle5 = new Circle(5);
+            Circle circle6= new Circle(6);
+            Console.WriteLine("The area of a circle with a radius of 5 is; " + circle5.getArea());
+            Console.WriteLine("The area of a circle with a radius of 6 is; " + circle6.getArea());
+        }
+        public class Circle
+        {
+            double Radius;
+
+            public Circle(double Radius) 
+            { 
+                this.Radius = Radius;
+            }
+            public double getArea()
+            {
+                double Area = Math.PI * Radius * Radius;
+                return Area;
+            }
+
+        }
+    }
+}
